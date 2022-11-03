1b7608a725071cc5b4534726bd4be5250d917bc9
Franz-Filip Schörghuber <S1910307091@fhooe.at>
2022-11-03 14:34:37 +0100

    Add project files.

---
+++ SAP.MVVC/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SAP.MVVC {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+        public UserViewModel CharViewModel { get; set; }
+        public MainWindow() {
+            CharViewModel = new UserViewModel();
+            UpdateViewModel();
+            InitializeComponent();
+        }
+
+        private void addToList(object sender, RoutedEventArgs e) {
+            CharViewModel.AddUser();
+            UpdateViewModel();
+        }
+
+        private void UpdateViewModel() {
+            DataContext = this.CharViewModel;
+        }
+
+    }
+}
---
+++ SAP.MVVC/UserViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.ObjectModel;
+
+namespace SAP.MVVC {
+    public class UserViewModel {
+        public ObservableCollection<User> Users { get; set; }
+        public User CurrentUser { get; set; }
+
+        public UserViewModel() {
+            Users = new ObservableCollection<User>() {
+                new User{UserId = 1,FirstName="Raj",LastName="Beniwal",City="Delhi",State="DEL",Country="INDIA"},
+                new User{UserId=2,FirstName="Mark",LastName="henry",City="New York", State="NY", Country="USA"},
+                new User{UserId=3,FirstName="Mahesh",LastName="Chand",City="Philadelphia", State="PHL", Country="USA"},
+                new User{UserId=4,FirstName="Vikash",LastName="Nanda",City="Noida", State="UP", Country="INDIA"},
+                new User{UserId=5,FirstName="Harsh",LastName="Kumar",City="Ghaziabad", State="UP", Country="INDIA"},
+            };
+        }
+
+        public void AddUser() {
+            int value = Users.Count + 1;
+            Users.Add(new User { UserId = value, FirstName = "TestUser" + value, LastName = "TestUserLastname", City = "Irgendwo", Country = "Aserbaidschan", State = "NCY" });
+        }
+    }
+}
